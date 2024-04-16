14d056a1fa9b8da979984d9a03b05f514ecd26a6
mehedihasan8 <mehedihasanfoysal22@gmail.com>
2024-04-16 23:39:40 +0600

    today task done

--- src/app/modules/Schedules/schedules.service.ts
+++ src/app/modules/Schedules/schedules.service.ts
@@ -1,5 +1,30 @@
+import { addHours, format } from "date-fns";
+
 const insertIntoDB = (payload: any) => {
-  console.log("clidlsjfdl", payload);
+  const { startDate, endDate, startTime, endTime } = payload;
+  const currentDate = new Date(startDate);
+  const lastDate = new Date(endDate);
+
+  while (currentDate <= lastDate) {
+    const startDateTime = new Date(
+      addHours(
+        `${format(currentDate, "yyyy-MM-dd")}`,
+        Number(startTime.split(":")[0])
+      )
+    );
+
+    const endDateTime = new Date(
+      addHours(
+        `${format(lastDate, "yyyy-MM-dd")}`,
+        Number(endTime.split(":")[0])
+      )
+    );
+    console.log(endDateTime);
+
+    while (startDateTime <= endDateTime) {
+      // next module.
+    }
+  }
 };
 export const schedulesService = {
   insertIntoDB,
