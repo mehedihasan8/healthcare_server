5350500196ab18df04ac4f2aab682e9a7ed4fd4c
mehedihasan8 <mehedihasanfoysal22@gmail.com>
2024-04-19 22:49:22 +0600

    add schedules time

--- src/app/modules/Schedules/schedules.service.ts
+++ src/app/modules/Schedules/schedules.service.ts
@@ -1,9 +1,10 @@
-import { addHours, format } from "date-fns";
+import { addHours, addMinutes, format } from "date-fns";
 
 const insertIntoDB = (payload: any) => {
   const { startDate, endDate, startTime, endTime } = payload;
   const currentDate = new Date(startDate);
   const lastDate = new Date(endDate);
+  const interverTime = 30;
 
   while (currentDate <= lastDate) {
     const startDateTime = new Date(
@@ -15,14 +16,20 @@ const insertIntoDB = (payload: any) => {
 
     const endDateTime = new Date(
       addHours(
-        `${format(lastDate, "yyyy-MM-dd")}`,
+        `${format(currentDate, "yyyy-MM-dd")}`,
         Number(endTime.split(":")[0])
       )
     );
+
+    console.log(startDateTime);
     console.log(endDateTime);
 
-    while (startDateTime <= endDateTime) {
-      // next module.
+    while (startDateTime < endDateTime) {
+      const scheduleData = {
+        startDateTime: startDateTime,
+        endDateTime: addMinutes(startDateTime, interverTime),
+      };
+      console.log(scheduleData);
     }
   }
 };
