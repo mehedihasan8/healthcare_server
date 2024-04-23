ae7e620d45ecf46a9f14efbc25254ffcc713772d
mehedihasan8 <mehedihasanfoysal22@gmail.com>
2024-04-23 22:16:26 +0600

    fixed doctor schedules user request

--- src/app/modules/DoctorSchedule/doctorSchedules.controller.ts
+++ src/app/modules/DoctorSchedule/doctorSchedules.controller.ts
@@ -4,17 +4,19 @@ import sendResponse from "../../../shared/sendResponse";
 import httpStatus from "http-status";
 import { doctorSchedulesService } from "./doctorSchedule.service";
 
-const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
-  const user = req.user;
-  const result = await doctorSchedulesService.insertIntoDB(user, req.body);
+const insertIntoDB = catchAsync(
+  async (req: Request & { user?: any }, res: Response) => {
+    const user = req.user;
+    const result = await doctorSchedulesService.insertIntoDB(user, req.body);
 
-  sendResponse(res, {
-    statusCode: httpStatus.OK,
-    success: true,
-    message: "Doctor schedule create successfully!",
-    data: result,
-  });
-});
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: "Doctor schedule create successfully!",
+      data: result,
+    });
+  }
+);
 
 export const doctorSchedulesController = {
   insertIntoDB,
